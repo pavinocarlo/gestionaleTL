14bdb53e0e53754bba9757d805555dc4835ab39b
alessio <alessio@DESKTOP-P6TQ6T6.localdomain>
2021-04-15 10:30:24 +0200

    insert abitazione

--- GestionaleTL/src/it/exolab/gestionaleTL/controller/AbitazioneController.java
+++ GestionaleTL/src/it/exolab/gestionaleTL/controller/AbitazioneController.java
@@ -3,12 +3,33 @@ package it.exolab.gestionaleTL.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import it.exolab.gestionaleTL.crud.AbitazioneCrud;
+import it.exolab.gestionaleTL.exception.AlreadyExistException;
+import it.exolab.gestionaleTL.exception.GenericException;
+import it.exolab.gestionaleTL.exception.InvalidFieldException;
+import it.exolab.gestionaleTL.model.Abitazione;
+
+
 public class AbitazioneController extends BaseController {
+	
+	private AbitazioneCrud abitazioneCrud = new AbitazioneCrud();
 	
 	public AbitazioneController(HttpServletRequest request, HttpServletResponse response) {
 		
 		super(request, response);
 		
+	}
+	
+	
+	protected void doInsert(HttpServletRequest request, HttpServletResponse response) throws GenericException, AlreadyExistException, InvalidFieldException {
+			
+			Abitazione abitazione = new Abitazione(request.getParameter("interno"),
+												request.getParameter("indirizzo"));
+								
+//			if(validate(user)) {
+				
+				abitazioneCrud.insert(abitazione);
+//			}
 	}
 
 }
--- GestionaleTL/src/it/exolab/gestionaleTL/mapper/AbitazioneMapper.java
+++ GestionaleTL/src/it/exolab/gestionaleTL/mapper/AbitazioneMapper.java
@@ -1,9 +1,11 @@
 package it.exolab.gestionaleTL.mapper;
 
 import it.exolab.gestionaleTL.model.Abitazione;
+import it.exolab.gestionaleTL.model.User;
 
 public interface AbitazioneMapper {
 	
 	public Abitazione find(Integer id);
+	public void insert(Abitazione abitazione);
 
 }
