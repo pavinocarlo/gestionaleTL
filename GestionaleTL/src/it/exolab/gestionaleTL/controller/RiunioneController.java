20c0c3961de47a36c65e5930deec72dbb91a6d75
alessio <alessio@DESKTOP-P6TQ6T6.localdomain>
2021-04-15 11:33:02 +0200

    riunione insert e update

--- GestionaleTL/src/it/exolab/gestionaleTL/controller/RiunioneController.java
+++ GestionaleTL/src/it/exolab/gestionaleTL/controller/RiunioneController.java
@@ -1,13 +1,52 @@
 package it.exolab.gestionaleTL.controller;
 
+import java.sql.Timestamp;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class RiunioneController extends BaseController {
+import it.exolab.gestionaleTL.crud.RiunioneCrud;
+import it.exolab.gestionaleTL.exception.AlreadyExistException;
+import it.exolab.gestionaleTL.exception.GenericException;
+import it.exolab.gestionaleTL.exception.InvalidFieldException;
+import it.exolab.gestionaleTL.model.Abitazione;
+import it.exolab.gestionaleTL.model.Riunione;
 
-public RiunioneController(HttpServletRequest request, HttpServletResponse response) {
+public class RiunioneController extends BaseController {
+	
+	RiunioneCrud riunioneCrud =  new RiunioneCrud();
+
+	public RiunioneController(HttpServletRequest request, HttpServletResponse response) {
+			
+			super(request, response);
+			
+		}
+	
+	protected void doInsert(HttpServletRequest request, HttpServletResponse response) throws GenericException, AlreadyExistException, InvalidFieldException {
 		
-		super(request, response);
-		
+		Riunione riunione = new Riunione((Timestamp.valueOf("" + request.getParameter("data_comunicazione") + "00:00:00")),
+										(Timestamp.valueOf("" + request.getParameter("data_riunione") + "00:00:00")),
+										request.getParameter("ordine_del_giorno"),
+										request.getParameter("luogo"));
+							
+//		if(validate(user)) {
+			
+		riunioneCrud.insert(riunione);
+//		}
 	}
+	
+	protected void doUpdate(HttpServletRequest request, HttpServletResponse response) throws GenericException, AlreadyExistException, InvalidFieldException {
+			
+			Riunione riunione = new Riunione((Timestamp.valueOf("" + request.getParameter("data_comunicazione") + "00:00:00")),
+											(Timestamp.valueOf("" + request.getParameter("data_riunione") + "00:00:00")),
+											(Timestamp.valueOf("" + request.getParameter("inizio_riunione") + "00:00:00")),
+											(Timestamp.valueOf("" + request.getParameter("fine_riunione") + "00:00:00")),
+											request.getParameter("ordine_del_giorno"),
+											request.getParameter("luogo"));
+								
+	//		if(validate(user)) {
+				
+			riunioneCrud.update(riunione);
+	//		}
+		}
 }
--- GestionaleTL/src/it/exolab/gestionaleTL/crud/RiunioneCrud.java
+++ GestionaleTL/src/it/exolab/gestionaleTL/crud/RiunioneCrud.java
@@ -1,12 +1,34 @@
 package it.exolab.gestionaleTL.crud;
 
 
+import it.exolab.gestionaleTL.exception.AlreadyExistException;
+import it.exolab.gestionaleTL.exception.GenericException;
+import it.exolab.gestionaleTL.exception.InvalidFieldException;
+
 import it.exolab.gestionaleTL.mapper.RiunioneMapper;
 
 import it.exolab.gestionaleTL.model.Riunione;
 import it.exolab.gestionaleTL.mybatis.SqlMapFactory;
 
 public class RiunioneCrud {
+	
+	public void insert(Riunione riunione) throws GenericException, AlreadyExistException, InvalidFieldException{
+
+		SqlMapFactory.instance().openSession();
+		RiunioneMapper mapper =  SqlMapFactory.instance().getMapper(RiunioneMapper.class);
+		mapper.insert(riunione);
+		SqlMapFactory.instance().commitSession();
+		SqlMapFactory.instance().closeSession();
+	}
+	
+	public void update(Riunione riunione) throws GenericException, AlreadyExistException, InvalidFieldException{
+
+		SqlMapFactory.instance().openSession();
+		RiunioneMapper mapper =  SqlMapFactory.instance().getMapper(RiunioneMapper.class);
+		mapper.update(riunione);
+		SqlMapFactory.instance().commitSession();
+		SqlMapFactory.instance().closeSession();
+	}
 	
 	public Riunione find(Integer id){
 		SqlMapFactory.instance().openSession();
